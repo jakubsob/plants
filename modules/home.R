59bee513e7aa482f08fd4efcfe9b731afa4846a2
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2021-05-13 00:06:53 +0200

    Update home screen info

--- modules/home.R
+++ modules/home.R
@@ -11,30 +11,29 @@ box::use(
 )
 
 #' @export
-ui <- function(id) {
-  ns <- NS(id)
-  tagList(
-    ui_utils$card(h1("Plants"), home_text()),
-    reactOutput(ns("cards")),
-  )
-}
-
-home_text <- function() {
-    
+ui <- function() {
   div(
-    class = "flex-container",
+    class = "card ms-depth-16 ms-sm12 ms-xl12 flex-container",
     div(
       class = "flex-child",
-      tagList(
-        "Discover your plants and check weather in their places of origin.",
+      Stack(
+        horizontal = FALSE,
+        tokens = list(childrenGap = 10),      
+        Text(variant = "large", h1("Plants"), block = TRUE),
+        span("Have you ever wanted to have a better relationship with your plants?"),
+        span("Learn about your plants so you can provide them better conditions"),
         br(),
-        "Search for your plant using Add button in pane on the left.",
-        br(),
-        "Navigate through tabs using menu in the header."
+        h3("How to use"),
+        span("Use sidebar on the left to search for your plants in the database. Once added they will be listed in the menu"),
+        span("Learn about your plants in ", strong(Icon(iconName = "ContactInfo")), " Info tab"),
+        span("To see where your plant originates from see ", strong(Icon(iconName = "Nav2DMapView")), " Map tab.",
+             "This is where you can look up weather from places of origin to provide conditions and make your plant feel like at home"
+        ),
+        span("Once plants are added you can download the list and upload it next time in order not to search the plants once again")
       )
     ),
     div(
-      class = "flex-child",
+      class = "flex-push",
       img(src = "www/plant.png", class = "home-img")
     )
   )
